3d84565e6bff1f0fd4d94de0a88e213be199b4f3
vee-el <veronicalennon@live.com>
2023-02-21 19:14:24 +0000

    Day 1

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 3000;
 
 //TODO
 
+app.get('/musicians', async (req, res) => {
+    const musiciansAll = await Musician.findAll();
+    res.json(musiciansAll);
+})
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
