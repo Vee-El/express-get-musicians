fb4171d0b1c14bff4a8d7819a6dd543aa320a5ec
vee-el <veronicalennon@live.com>
2023-02-21 19:18:04 +0000

    Day 2

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ app.get('/musicians', async (req, res) => {
     res.json(musiciansAll);
 })
 
+app.get('/musicians/:id', async (req, res) => {
+
+    try {
+        const musiciansID = await Musician.findByPk(req.params.id);
+
+        if (!musiciansID) {
+            throw new Error('Musician not found');
+        } 
+        
+        res.json(musiciansID);
+    
+    } catch (error) {
+
+        res.status(404).send({ error: error.message });
+    }
+});
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
